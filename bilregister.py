14f747ba13fc94b0b377e15b72953ac58b8f98f6
klas raug <herman.johansson@skola.taby.se>
2022-01-18 09:35:00 +0100

    Co-authored-by: Wodshaix <Wodshaix@users.noreply.github.com> Co-authored-by: ruben1232 <ruben1232@users.noreply.github.com>

--- bilregister.py
+++ bilregister.py
@@ -1,3 +1,8 @@
+from typing import MutableMapping
+
+id = 0
+
+
 class Car():
     '''
     En klass som håller reda på några egenskaper hos en bil.
@@ -25,11 +30,43 @@ class Car():
         '''
         self.brand = new_brand
 
+    def set_color(self, new_color):
+        self.color = new_color
+
+    def set_mileage(self, new_mileage):
+        self.mileage = new_mileage
+
+    def get_mileage(self):
+        return self.mileage
+
 
 # ----------Huvudprogram----------
 # Nu när klassen finns kan vi skapa objekt (variabler) med denna typ.
 # Dessa objekt har också tillgång till klassens metoder (funktioner).
+
 a_car = Car('Volvo', 'Blå', 1587)
-a_car.get_brand()
-a_car.set_brand('Renault')
-a_car.get_brand()
+b_car = Car('BMW', 'Svart', 1864)
+c_car = Car('Ferrari', 'Röd', 1687)
+d_car = Car('Tesla', 'Vit', 1422)
+
+my_cars = [a_car, b_car, c_car, d_car]
+
+print()
+
+svar = ""
+while svar != 5:
+    svar = int(
+        input("Vilken bil vill du inspektera [1] [2] [3] [4], för att avsluta skriv [5]"))
+    if svar >= 1 and svar <= 4:
+        svar2 = input(
+            f"Du valde bil {svar}\nVilken attribut vill du ta reda på [brand] [color] [mileage]")
+        if svar2 == "brand":
+            print(my_cars[svar-1].brand)
+        elif svar2 == "color":
+            print(my_cars[svar-1].color)
+        elif svar2 == "mileage":
+            print(my_cars[svar-1].mileage)
+        else:
+            print("felaktigt val lol")
+    elif svar == 5:
+        print("HEJDÅ")
